3bd42636427c9af19d7f569f99ce223eabc75895
SamucaChoque19 <102567695+SamucaChoque19@users.noreply.github.com>
2022-08-24 12:44:00 -0300

    Add files via upload

---
+++ notas.js
@@ -0,0 +1,9 @@
+let nota1 = 8;
+let nota2 = 7;
+let nota3 = 7;
+let nota4 = 8;
+let media = ((nota1+nota2+nota3+nota4)/4);
+let resultado = media >= 7;
+
+console.log(media);
+console.log(resultado, 'aluno aprovado');
